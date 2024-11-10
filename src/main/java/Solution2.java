0a8cb88061bcc4b3e839ebccd1a0981ffba25b14
15358 <1535874218@qq.com>
2024-11-10 14:51:30 +0800

    3

--- src/main/java/Solution2.java
+++ src/main/java/Solution2.java
@@ -1,21 +1,4 @@
-/**
- * @description:
- *
- * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
- *
- *
- * 示例 1：
- *
- * 输入：s = "bcabc"
- * 输出："abc"
- * 示例 2：
- *
- * 输入：s = "cbacdcbc"
- * 输出："acdb"
- *
- * 1 <= s.length <= 104
- * s 由小写英文字母组成
- */
+
 public class Solution2 {
     public String removeDuplicateLetters(String s) {
         boolean[] vis = new boolean[26]; // 修改数组大小为26，因为'a'-'z'共26个字母
